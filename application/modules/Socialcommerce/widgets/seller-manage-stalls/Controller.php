ff25b8fc28dd7659417c688e8a053395d787e302
Nguyễn Chí Thanh <thanhnc@younetco.com>
2016-12-10 23:29:58 +0700

    update count

--- application/modules/Socialcommerce/widgets/seller-manage-stalls/Controller.php
+++ application/modules/Socialcommerce/widgets/seller-manage-stalls/Controller.php
@@ -8,11 +8,19 @@
  */
 class Socialcommerce_Widget_SellerManageStallsController extends Engine_Content_Widget_Abstract
 {
+    protected $_childCount;
+
     public function indexAction()
     {
         // Return if guest try to access to create link.
         $viewer = Engine_Api::_()->user()->getViewer();
         $paginator = Engine_Api::_()->getDbTable('stalls', 'socialcommerce')->getStallsPaginator(array('owner_id' => $viewer->getIdentity()));
         $this->view->paginator = $paginator;
+        $this->_childCount = $paginator->getTotalItemCount();
+    }
+
+    public function getChildCount()
+    {
+        return $this->_childCount;
     }
 }
